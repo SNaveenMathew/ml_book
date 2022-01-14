edc03c857f8d0beeec77f374f09844701edd65de
Naveen Mathew <naveenmathewnathan.s@gmail.com>
2022-01-14 04:44:00 +0000

    Modularized model definition

--- quadratic_model.py
+++ quadratic_model.py
@@ -33,39 +33,52 @@ class ConstantTensorConstraint(keras.constraints.Constraint):
 	def get_config(self):
 		return {'t': self.t}
 
-# layer1 = BatchNormalization()
-layer2 = Dense(
-	input_dim = 1,
-	units = 2,
-	use_bias = True,
-	# kernel_initializer = GlorotNormal(seed = 1),
-	kernel_initializer = HeNormal(seed = 1),
-	bias_initializer = ConstantTensorInitializer(biases),
-	bias_constraint = ConstantTensorConstraint(biases),
-	activation = "sigmoid"
-)
-# layer2 = Dense(
-# 	2,
-# 	use_bias = True,
-# 	activation = "sigmoid"
-# )
-# layer3 = BatchNormalization()
-layer4 = Dense(
-	1,
-	use_bias = True,
-	activation = "linear"
-)
+def get_model(bias_constraint = True, batch_normalization = False, learning_rate = 0.01 * np.sqrt(10)):
+	if batch_normalization:
+		layer1 = BatchNormalization()
 
-# model = Sequential(
-# 	[layer1, layer2, layer3, layer4]
-# )
-model = Sequential(
-	[layer2, layer4]
-)
+	if bias_constraint:
+		layer2 = Dense(
+			input_dim = 1,
+			units = 2,
+			use_bias = True,
+			# kernel_initializer = GlorotNormal(seed = 1),
+			kernel_initializer = HeNormal(seed = 1),
+			bias_initializer = ConstantTensorInitializer(biases),
+			bias_constraint = ConstantTensorConstraint(biases),
+			activation = "sigmoid"
+		)
+	else:
+		layer2 = Dense(
+			input_dim = 1,
+			units = 2,
+			use_bias = True,
+			activation = "sigmoid",
+			kernel_initializer = HeNormal(seed = 1)
+		)
 
-optimizer = Adam(learning_rate = 0.01 * np.sqrt(10))
-# reduce_lr = ReduceLROnPlateau()
-model.compile(loss = 'mean_squared_error', optimizer = optimizer)
+	if batch_normalization:
+		layer3 = BatchNormalization()
+
+	layer4 = Dense(
+		1,
+		use_bias = True,
+		activation = "linear"
+	)
+
+	if batch_normalization:
+		model = Sequential(
+			[layer1, layer2, layer3, layer4]
+		)
+	else:
+		model = Sequential(
+			[layer2, layer4]
+		)
+
+	optimizer = Adam(learning_rate = learning_rate)
+	# reduce_lr = ReduceLROnPlateau()
+	model.compile(loss = 'mean_squared_error', optimizer = optimizer)
+	return model
 
 def train(model, epochs = 5000000):
 	for epoch in range(epochs):
